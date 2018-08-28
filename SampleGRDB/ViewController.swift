8e211bd88c8dbcc2d1c1a3d83ebe3c6c9bdf29c7
Sung Yu <hwchon1234@naver.com>
2018-08-28 11:51:49 +0900

    create, read, insert 코드 추가 -> sql로 실행하는 코드

--- SampleGRDB/ViewController.swift
+++ SampleGRDB/ViewController.swift
@@ -12,7 +12,12 @@ import GRDB
 class ViewController: UIViewController {
     
     var dbQueue: DatabaseQueue!
-
+    
+    let usersTable = "users"
+    let id = "id"
+    let name = "name"
+    let email = "email"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,14 +30,58 @@ class ViewController: UIViewController {
             print(error)
         }
     }
-
+    
     @IBAction func createTable(_ sender: Any) {
+        print("CREATE TAPPED")
+        do {
+            try dbQueue.write { db in
+                try db.create(table: usersTable) { table in
+                    table.autoIncrementedPrimaryKey(id).primaryKey()
+                    table.column(name)
+                    table.column(email).unique()
+                }
+            }
+        } catch {
+            print(error)
+        }
     }
     
     @IBAction func insertUser(_ sender: Any) {
+        do {
+            try dbQueue.write { db in
+                try db.execute("""
+                    INSERT INTO users (name, email) VALUES (?, ?);
+                """, arguments: ["User1", "a@a.com"])
+            }
+        } catch {
+            print(error)
+        }
     }
     
     @IBAction func listUsers(_ sender: Any) {
+        do {
+            try dbQueue.read { db in
+//                let rows = try Row.fetchCursor(db, "SELECT * FROM users")
+//                while let row = try rows.next() {
+//                    let name: String = row["name"]
+//                    let email: String = row["email"]
+//
+//                    print(name)
+//                    print(email)
+//                }
+                
+                let rows = try Row.fetchAll(db, "SELECT * FROM users")
+                for row in rows {
+                    let name: String = row["name"]
+                    let email: String = row["email"]
+                    
+                    print(name)
+                    print(email)
+                }
+            }
+        } catch {
+            print(error)
+        }
     }
     
     @IBAction func updateUser(_ sender: Any) {
